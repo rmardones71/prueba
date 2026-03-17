82a835ba0d0d8b0416beb427f32f6dc9755a7239
rmardones <soporte@wprochile.com>
2026-03-17 11:32:49 -0300

    Primer commit

---
+++ src/components/AppSidebar.js
@@ -0,0 +1,98 @@
+/**
+ * AppSidebar Component
+ *
+ * Collapsible navigation sidebar with branding, menu items, and toggle controls.
+ *
+ * Features:
+ * - Redux-controlled visibility state
+ * - Unfoldable/narrow mode for more screen space
+ * - Brand logo with full and narrow variants (conmutación por CSS)
+ * - Close button for mobile devices
+ * - Footer with toggle button
+ * - Dark color scheme
+ * - Fixed positioning
+ *
+ * @component
+ * @example
+ * return (
+ *   <AppSidebar />
+ * )
+ */
+
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import {
+  CCloseButton,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarFooter,
+  CSidebarHeader,
+  CSidebarToggler,
+} from '@coreui/react'
+
+import { AppSidebarNav } from './AppSidebarNav'
+import navigation from '../_nav'
+
+// Importa tu logo desde src/assets (Vite hará bundling/optimización)
+import logoUcm from 'src/assets/logo_ucm.png'
+
+// Importa los estilos de conmutación full/narrow
+import 'src/styles/sidebar-brand.css'
+
+const AppSidebar = () => {
+  const dispatch = useDispatch()
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+
+  return (
+    <CSidebar
+      className="border-end"
+      colorScheme="dark"
+      position="fixed"
+      unfoldable={unfoldable}
+      visible={sidebarShow}
+      onVisibleChange={(visible) => {
+        dispatch({ type: 'set', sidebarShow: visible })
+      }}
+    >
+      <CSidebarHeader className="border-bottom">
+        <CSidebarBrand to="/" className="sidebar-brand">
+          {/* Variante FULL (sidebar expandido) */}
+          <img
+            className="sidebar-brand-full"
+            src={logoUcm}
+            alt="UCM - Unidad Coronaria Móvil"
+            height={32}
+            style={{ width: 'auto', display: 'block' }}
+          />
+
+          {/* Variante NARROW (sidebar angosto) */}
+          <img
+            className="sidebar-brand-narrow"
+            src={logoUcm}
+            alt="UCM"
+            height={32}
+            style={{ width: 'auto', display: 'block' }}
+          />
+        </CSidebarBrand>
+
+        <CCloseButton
+          className="d-lg-none"
+          dark
+          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+        />
+      </CSidebarHeader>
+
+      <AppSidebarNav items={navigation} />
+
+      <CSidebarFooter className="border-top d-none d-lg-flex">
+        <CSidebarToggler
+          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+        />
+      </CSidebarFooter>
+    </CSidebar>
+  )
+}
+
+export default React.memo(AppSidebar)
